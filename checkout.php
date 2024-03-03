a36fcc1dcefc1a6d1fc4d290b4de97e5e827de1b
XXoussam <saoudi.oussama@esprit.tn>
2024-03-03 01:42:20 +0100

    first commit

---
+++ checkout.php
@@ -0,0 +1,39 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+$stripe_secret_key = "sk_test_51NAJo2F0vws68R2QGzeTwdJf6KTZ5vrlgQkOvyHOIZziSkoXVo6NL4QGsri9wm3bK3iXnVOjGTTYU59GmnRupyWR00hMV17ROf";
+
+\Stripe\Stripe::setApiKey($stripe_secret_key);
+
+$checkout_session = \Stripe\Checkout\Session::create([
+    "mode" => "payment",
+    "success_url" => "http://localhost/success.php",
+    "cancel_url" => "http://localhost/index.php",
+    "locale" => "auto",
+    "line_items" => [
+        [
+            "quantity" => 1,
+            "price_data" => [
+                "currency" => "usd",
+                "unit_amount" => 2000,
+                "product_data" => [
+                    "name" => "T-shirt"
+                ]
+            ]
+        ],
+        [
+            "quantity" => 2,
+            "price_data" => [
+                "currency" => "usd",
+                "unit_amount" => 700,
+                "product_data" => [
+                    "name" => "Hat"
+                ]
+            ]
+        ]        
+    ]
+]);
+
+http_response_code(303);
+header("Location: " . $checkout_session->url);
